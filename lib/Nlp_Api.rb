7563c67f41b8a9e29299812d1c238a103e5dda27
chad metcalf <Chad@databasically.com>
2011-10-11 13:18:52 -0500

    cleaning up

--- lib/Nlp_Api.rb
+++ lib/Nlp_Api.rb
@@ -1,8 +1,8 @@
 module NlpApi
   
-  autoload :Request "Nlp_Api/request"
-  autoload :Response "Nlp_Api/response"
-  autoload :Api "Nlp_Api/api"
+  autoload :Request   "Nlp_Api/request"
+  autoload :Response  "Nlp_Api/response"
+  autoload :Api       "Nlp_Api/api"
   
   
   
--- lib/Nlp_Api/sample run.rb
+++
@@ -1,4 +0,0 @@
-
-require ./lib/Nlp_Api.rb
-
-Nlp_Api.Request.build(2,30)
