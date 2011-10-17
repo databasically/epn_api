349776ef5126a138a7e95aef85f45990eede9ddb
chad metcalf <Chad@databasically.com>
2011-10-17 11:52:29 -0500

    name change nlp to epn

--- lib/Epn_Api.rb
+++ lib/Epn_Api.rb
@@ -2,9 +2,9 @@ require 'nokogiri'
 
 module EpnApi
   
-  autoload :Paper,  "./lib/evp_api/paper"
-  autoload :Api,    "./lib/evp_api/api"
-  autoload :ApiDoc, "./lib/evp_api/api_doc"
+  autoload :Paper,  "./lib/epn_api/paper"
+  autoload :Api,    "./lib/epn_api/api"
+  autoload :ApiDoc, "./lib/epn_api/api_doc"
   
   
   
