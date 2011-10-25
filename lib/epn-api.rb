60ba26b8b168ffdf8342df67513b183059c7a1c9
chad metcalf <Chad@databasically.com>
2011-10-25 09:17:08 -0500

    fixed relative paths bug

--- lib/epn-api.rb
+++ lib/epn-api.rb
@@ -3,9 +3,9 @@ require 'net/http'
 
 module EpnApi
   
-  autoload :Paper,  "./lib/epn_api/paper"
-  autoload :Api,    "./lib/epn_api/api"
-  autoload :ApiDoc, "./lib/epn_api/api_doc"
+  autoload :Paper,  "epn_api/paper"
+  autoload :Api,    "epn_api/api"
+  autoload :ApiDoc, "epn_api/api_doc"
   
   
   
