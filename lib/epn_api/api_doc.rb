db0ff1ff9efbe478ad1006d73f0884eb1ec2ce32
Wes Garrison <wes@wesgarrison.us>
2011-10-31 13:26:13 -0500

    Add lowercase directory name files

---
+++ lib/epn_api/api_doc.rb
@@ -0,0 +1,91 @@
+module EpnApi
+  class ApiDoc
+    include Api
+    
+    attr_accessor :grade, :name, :annualqp, :recycledcontent, 
+                  :bod, :cod, :greenhouse_gas, :hap, :nox, :particulates,
+                  :purchased_energy,:solid_waste, :so2, :net_energy, :trs,
+                  :tss, :voc, :wastewater, :wood_use
+    
+    
+    TREES_PER_TON_OF_WOOD_USE = 6.93
+    
+    def to_epn_xml( paper )
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.papers('client' => self.get_api_code) do
+          xml.group('id' => '0', 'name' => "default") do
+            xml.paper('name' => 'PaperApi') do
+              xml.grade_ paper.grade
+              xml.name_ paper.name
+              xml.annualqp_ paper.annualqp['amount']
+              xml.qpunits('value' => "#{paper.annualqp["qpunits"]}")
+              xml.recycledcontent_ paper.recycledcontent
+            end
+          end
+        end
+      end
+      builder.to_xml
+    end
+    
+    def get_api_code
+      api_code_doc = ''
+      api_code_doc = YAML::load_file( './config/api_code.yml' )
+      return api_code_doc['epn_api_code']
+    end
+
+    def from_epn_api(response_body)
+      doc = Nokogiri::HTML(response_body)
+      doc.xpath('//paper/grade').each do |node|
+        self.grade= node.text.strip
+      end
+      doc.xpath('//paper/name').each do |node|
+        self.name = node.text.strip
+      end
+      doc.xpath('//paper/annualqp').each do |node|
+        self.annualqp ||= {}
+        self.annualqp["amount"] = node.text.strip
+      end
+      doc.xpath('//paper/qpunits').each do |node|
+        self.annualqp ||= {}
+        self.annualqp["qpunits"] = node.attributes['value'].value.strip
+      end
+      doc.xpath('//paper/recycledcontent').each do |node|
+        self.recycledcontent = node.text.strip
+      end
+      doc.xpath('//eparam').each do |node|
+        node_name = node.xpath('name').text.strip
+
+        hash = {"unit" => node.xpath('unit').text.strip, "value" => node.xpath('value').text.strip}
+        self.send("#{node_name}=", hash)
+      end
+      nil
+    end
+    
+    def check_epn( request_xml )
+      response_doc = self.get_epn_response_doc( request_xml )
+      self.accept_status( response_doc )
+      self.from_epn_api( response_doc.body )
+    end
+    
+    def do_conversions!( paper )
+      unit = paper.unit_check
+      
+      paper.trees = ((self.wood_use["value"].to_f/unit) * TREES_PER_TON_OF_WOOD_USE)
+      paper.water = (self.wastewater["value"].to_f/unit)
+      paper.energy = (self.net_energy["value"].to_f/unit)
+      paper.solid_waste = (self.solid_waste["value"].to_f/unit)
+      paper.greenhouse_gas = (self.greenhouse_gas["value"].to_f/unit)
+      paper.annualqp["amount"] = (paper.annualqp["amount"]/unit)
+      return nil
+    end
+    
+    def epn_response!( paper )
+      request_xml = self.to_epn_xml( paper )
+      self.check_epn( request_xml )
+      self.do_conversions!( paper )
+      return paper
+    end
+
+
+  end
+end
---
+++ lib/epn_api/version.rb
@@ -0,0 +1,11 @@
+module EpnApi
+
+  module VERSION #:nodoc:
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 3
+    PRE = nil
+    STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
+  end
+
+end
